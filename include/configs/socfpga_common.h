bd6931ed9410e407b0eb394de024a69d2775b306
Maciej Mikunda <mmikunda@antmicro.com>
2017-08-28 15:30:03 +0200

    configs: socfpga common: remove unnecessary defines
    We don't want to enable UBIFS support from common header level.

--- include/configs/socfpga_common.h
+++ include/configs/socfpga_common.h
@@ -284,13 +284,6 @@ unsigned int cm_get_qspi_controller_clk_hz(void);
 				"-@1536k(UBI)\0"
 #endif
 
-/* UBI and UBIFS support */
-#if defined(CONFIG_CMD_SF) || defined(CONFIG_CMD_NAND)
-#define CONFIG_CMD_UBIFS
-#define CONFIG_RBTREE
-#define CONFIG_LZO
-#endif
-
 /*
  * SPL
  *
