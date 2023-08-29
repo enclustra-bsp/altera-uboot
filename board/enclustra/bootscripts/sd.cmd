c51962bf2e89b3741928a96066907b1b4493a9af
Andreas Buerkler <andreas.buerkler@enclustra.com>
2023-08-29 19:00:49 +0200

    Default U-Boot environment updated / boot scripts replaced

---
+++ board/enclustra/bootscripts/sd.cmd
@@ -0,0 +1,6 @@
+fatload mmc 0:1 ${ram_addr_bitstream} fpga.rbf
+fpga load 0 ${ram_addr_bitstream} 0x${filesize}
+bridge enable LWHPS-to-FPGA;
+fatload mmc 0:1 ${ram_addr_kernel} uImage
+fatload mmc 0:1 ${ram_addr_devicetree} devicetree.dtb
+bootm ${ram_addr_kernel} - ${ram_addr_devicetree}
--- include/configs/socfpga_mercury_aa1.h
+++ include/configs/socfpga_mercury_aa1.h
@@ -23,6 +23,35 @@
 #define CONFIG_SYS_BAUDRATE_TABLE {4800, 9600, 19200, 38400, 57600, 115200}
 #define CONFIG_SYS_NAND_U_BOOT_OFFS	0x100000
 
+/* U-Boot environment */
+#define SOCFPGA_BOOT_SETTINGS \
+    "qspi_offset_addr_spl=0x0\0" \
+    "qspi_offset_addr_u-boot=0x100000\0" \
+    "qspi_offset_addr_u-boot-env=0x180000\0" \
+    "qspi_offset_addr_boot-script=0x200000\0" \
+    "qspi_offset_addr_devicetree=0x280000\0" \
+    "qspi_offset_addr_bitstream=0x300000\0" \
+    "qspi_offset_addr_kernel=0x1000000\0" \
+    "qspi_offset_addr_rootfs=0x2000000\0" \
+    "size_spl=0x100000\0" \
+    "size_u-boot=0x80000\0" \
+    "size_u-boot-env=0x80000\0" \
+    "size_boot-script=0x80000\0" \
+    "size_devicetree=0x80000\0" \
+    "size_bitstream=0xD00000\0" \
+    "size_kernel=0x1000000\0" \
+    "size_rootfs=0x2000000\0" \
+    "ram_addr_rootfs=0x12000000\0" \
+    "ram_addr_kernel=0x11000000\0" \
+    "ram_addr_devicetree=0x10000000\0" \
+    "ram_addr_boot-script=0x10800000\0" \
+    "bootm_size=0x0a000000\0" \
+    "bootargs-mmc=earlycon console=ttyS0,115200 rw root=/dev/mmcblk0p2\0" \
+    "bootargs-qspi=earlycon console=ttyS0,115200 rw root=/dev/ram0\0" \
+    "bootargs=${bootargs-mmc}\0" \
+    "qspiload=setenv bootargs ${bootargs-qspi}; sf probe; sf read ${ram_addr_boot-script} ${qspi_offset_addr_boot-script} ${size_boot-script}\0" \
+    "qspiboot=source ${ram_addr_boot-script}\0"
+
 /*
  * L4 OSC1 Timer 0
  */
