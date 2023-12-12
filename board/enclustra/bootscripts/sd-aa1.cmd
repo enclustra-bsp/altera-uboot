173058ad94eb2a49a69356772cb982ff8300236d
Andreas Bürkler <andreas.buerkler@enclustra.com>
2023-12-12 19:08:48 +0100

    Fix bootscript for AA1 SD card boot

--- board/enclustra/bootscripts/sd-aa1.cmd
+++ board/enclustra/bootscripts/sd-aa1.cmd
@@ -1,8 +1,4 @@
 bridge enable
 fatload mmc 0:1 ${ram_addr_kernel} uImage
 fatload mmc 0:1 ${ram_addr_devicetree} devicetree.dtb
-fatload mmc 0:1 ${ram_addr_rootfs} uramdisk
-
-altera_set_storage QSPI
-
-bootm ${ram_addr_kernel} ${ram_addr_rootfs} ${ram_addr_devicetree}
+bootm ${ram_addr_kernel} - ${ram_addr_devicetree}
